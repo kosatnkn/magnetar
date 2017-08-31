0dab035be7bb3632223edfb1b75f5a5694e688d2
kosala <kosala@pickme.lk>
2017-08-31 09:55:14 +0530

    got App object in to test

--- test/tests/app_test.js
+++ test/tests/app_test.js
@@ -1,3 +1,7 @@
 QUnit.test( "hello test", function( assert ) {
     assert.ok( 1 == "1", "Passed!" );
 });
+
+QUnit.test( "hello test2", function( assert ) {
+    assert.ok( typeof App === "object", "Passed!" );
+});
